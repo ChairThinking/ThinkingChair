b4dbfe620ca52b7f30e79fdce0a997f6b83248a3
SY <choiseungyeon0814@gmail.com>
2025-06-03 19:26:19 +0900

    startSession 함수 및 세션 만료

--- kiosk/controllers/kioskController.js
+++ kiosk/controllers/kioskController.js
@@ -1,10 +1,12 @@
-const { v4: uuidv4 } = require('uuid');
+const { v4: uuidv4 } = require("uuid");
 
 exports.startSession = (req, res) => {
   const sessionId = uuidv4();
   const mockPayment = 100000;
   const entryTime = new Date();
+  //10분까지 물건 구매 없을 시 세션 자동 만료
+  const expireTime = new Date(Date.now() + 1 * 60 * 1000);
 
   // 실제 구현 시: DB 저장, RFID 인증, 문 개방, 결제 처리
-  res.json({ sessionId, paidAmount: mockPayment, entryTime });
+  res.json({ sessionId, paidAmount: mockPayment, entryTime, expireTime });
 };
