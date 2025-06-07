7720a74077c11495a11f38191d0501a1b806ec67
SY <choiseungyeon0814@gmail.com>
2025-06-07 17:58:02 +0900

    메인 엔트리 포인트

--- kiosk/app.js
+++ kiosk/app.js
@@ -1,23 +1,24 @@
 const express = require("express");
 const path = require("path");
+const http = require("http");
 const kioskRoutes = require("./routes/kioskRoutes");
+const initWebSocket = require("./websocket/kioskSocket");
 
 const app = express();
 const PORT = 3000;
+const server = http.createServer(app); // WebSocket 연동 가능한 서버
 
-// 미들웨어 설정
+// 미들웨어
 app.use(express.json());
-
-// API 라우터 연결
 app.use("/api", kioskRoutes);
-
-// 정적 파일 서빙 (프론트)
+//정적 파일 서빙(프론트)
 app.use(express.static(path.join(__dirname, "public")));
 app.use("/test", express.static(path.join(__dirname, "test")));
 
+// WebSocket 연결
+initWebSocket(server);
+
 // 서버 시작
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`Kiosk server running at http://localhost:${PORT}`);
 });
-
-
