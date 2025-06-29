70773e9b4f563345ebff2b20c8836d9c87c316e3
SY <choiseungyeon0814@gmail.com>
2025-06-30 00:43:51 +0900

    세션 시작 요청 라우터 설정

--- kiosk/routes/kioskRoutes.js
+++ kiosk/routes/kioskRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const kioskController = require("../controllers/kioskController");
 
 router.post("/session/start", kioskController.startSession);
+router.post("/api/refund", kioskController.processRefund);
 
 module.exports = router;
